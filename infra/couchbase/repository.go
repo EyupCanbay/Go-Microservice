5e5d4965babce4d0f1f1fc0eeca49a2542718543
EyupCanbay <eyupcanbay40@gmail.com>
2025-07-25 14:50:36 +0300

    Add finished couchbase connection

--- infra/couchbase/repository.go
+++ infra/couchbase/repository.go
@@ -3,21 +3,42 @@ package couchbase
 import (
 	"context"
 	"microservice/app/product"
+	"time"
 
 	"github.com/couchbase/gocb/v2"
+	"go.uber.org/zap"
 )
 
 type CouchbaseRepository struct {
+	cluster *gocb.Cluster
+	bucket *gocb.Bucket
 }
 
 func NewCouchbaseRepository() *CouchbaseRepository {
 
 	cluster, err := gocb.Connect("couchbasehttp://localhost",gocb.ClusterOpinions{
-		Authanticator: gocb.Authenticator{
-			Username: ""
-		}
+		TimeoutsConfig: gocb.TimeoutsConfig{
+			ConnectTimeout: 3 * time.Second,
+			KVTimeout: 3 * time.Second,
+			QueryTimeout: 3 * time.Second,
+		},
+		Authanticator: gocb.PasswordAuthenticator{
+			Username: "Administrator",
+			Password: "password",
+		},
+		Transcoder: gocb.NewJSONTranscoder(),
 	})
-	return &CouchbaseRepository{}
+	if err != nil {
+		zap.L().Fatal("failed connect to couchbase", zap.Error(err))
+	}
+	bucket := cluster.Bucket("products")
+	bucket.WaitUntilReady(3 * time.Second, &gocb.WaitUntilReadyOptions{})
+
+
+	return &CouchbaseRepository{
+		cluster: cluster,
+		bucket: bucket,
+	}
 }
 
 func (r *CouchbaseRepository) GetProduct(ctx context.Context, id string) (*product.Product, error)
